d2e4413821b81746e91f5e340b20c7683abe5ba1
dmercertaylor <dmercertaylor01@gmail.com>
2020-01-31 14:02:23 -0600

    finished zip router

--- server/routes/programs.router.js
+++ server/routes/programs.router.js
@@ -5,15 +5,30 @@ const router = express.Router();
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-    
-});
-
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-
+router.get('/:zip', async (req, res) => {
+    const query = `
+    SELECT "gpp".*
+    FROM "zips" JOIN  "gpp" ON "zips"."eia_state"="gpp"."eia_state"
+    WHERE "zips"."zip"=$1`
+    let programs = await pool.query(query, [req.params.zip]);
+    programs = programs.rows;
+    const dataToSend = [];
+    programs.forEach(program => {
+        let i = 0;
+        for(; i < dataToSend.length; i++){
+            if(dataToSend[i].name === program.utility_name) break;
+        }
+        if(i === dataToSend.length){
+            dataToSend.push({
+                name: program.utility_name,
+                programs: []
+            });
+        }
+        delete program.utility_name;
+        dataToSend[i].programs.push(program);
+    });
+    console.log(JSON.stringify(dataToSend));
+    res.send(dataToSend);
 });
 
 module.exports = router;
