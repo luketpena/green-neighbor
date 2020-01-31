01f465f79789ae8184d258ed92a6e5543140103f
Luke Peña <luketpena@gmail.com>
2020-01-31 16:08:29 -0600

    Program details unfold animation

--- src/components/UtilityPage/UtilityPage.js
+++ src/components/UtilityPage/UtilityPage.js
@@ -91,14 +91,20 @@ const ProgramCardBody = styled.div`
   h4 {
     margin: 0;
   }
+  height: ${props=>(props.detailsActive? '230' : '130')}px;
+  overflow: hidden;
+  transition: height .5s;
 `;
 
 const ProgramCardHeader = styled.div`
   background-color: var(--color-button-primary-main);
   color: white;
-  padding: 8px 16px;
+  display: flex;
+  align-items: center;
+  padding-left: 16px;
   position: relative;
   z-index: 2;
+  height: 30px;
 `;
 
 const ProgramCardMain = styled.div`
@@ -106,12 +112,13 @@ const ProgramCardMain = styled.div`
   box-shadow: 0 2px 4px 2px rgba(0,0,0,.5);
   position: relative;
   z-index: 1;
-  height: 64px;
+  height: 100px;
 `;
 
 const ProgramCardDetails = styled.div`
   background-color: yellow;
-  height: 64px;
+  height: 100px;
+  left: 0;
 `;
 
 function UtilityList () {
@@ -151,15 +158,15 @@ function UtilityCard (props) {
 
 function ProgramCard (props) {
 
-  
+  let [detailsActive, setDetailsActive] = useState(false);
 
   return (
-    <ProgramCardBody>
+    <ProgramCardBody detailsActive={detailsActive}>
       <ProgramCardHeader>
         <h4>{props.program.program_name}</h4>
       </ProgramCardHeader>
 
-      <ProgramCardMain>
+      <ProgramCardMain onClick={()=>setDetailsActive(!detailsActive)}>
 
       </ProgramCardMain>
 
