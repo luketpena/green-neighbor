ee7344977321070a73b7185877596118f32d5382
dmercertaylor <dmercertaylor01@gmail.com>
2020-02-03 12:52:03 -0600

    made programs not populate if name is not listed

---
+++ server/modules/gppColumns.js
@@ -0,0 +1,10 @@
+module.exports = [
+    'id', 'eiaid', 'utility_name', 'state', 'eia_state',
+    'program_id', 'program_name', 'wind', 'solar', 'bio',
+    'hydro', 'geo', 'other', 'cost_kwh', 'cost_range',
+    'contract_length', 'credit_yn', 'credit_kwh', 'blocks_available',
+    'block_size_kwh', 'block_cost', 'percentage_options', 'percentage_range',
+    'monthly_min', 'termination_fee', 'termination_cost', 'green_e',
+    'recs_retired', 'revenue_neutral', 'retail', 'waitlist',
+    'sign_up_text', 'sign_up_url', 'date_updated', 'production'
+];
