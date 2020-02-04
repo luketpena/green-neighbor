51f924e24b949b9a2192820fd473ddd2bb9cd862
Ben Stretar <ben.stretar@gmail.com>
2020-02-04 14:33:22 -0600

    About Page layed out. button pushes to home

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
+const Container = styled.div`
+width: 90%;
+height: 500px;
+margin: 0 auto;
+display: grid;
+grid-template-areas: "questions answers" ;
+grid-template-columns: 250px 1fr;
 
-const AboutPage = () => (
-  <div>
-    <div>
-      <p>
-        This about page is for anyone to read!
-      </p>
-    </div>
-  </div>
-);
+`;
 
-export default AboutPage;
+const QBox = styled.div`
+grid-area: questions;
+background-color: lightblue
+
+`;
+
+const ABox = styled.div`
+grid-area: answers;
+background-color: orange
+
+`;
+
+export default function AboutPage() {
+  const history = useHistory();
+
+  return(
+    <>
+    <button className="button-default" onClick={()=>history.push("/intro")}>return home</button>
+    <Container>
+      
+
+      <QBox className="container">
+        questions
+      </QBox>
+      <ABox className="container">
+        answers
+      </ABox>
+    </Container>
+    </>
+  )
+}
