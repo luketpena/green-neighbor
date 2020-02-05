b8dbe33f6a57ee9da07840f1d8076a427119486a
Luke Peña <luketpena@gmail.com>
2020-02-05 15:48:30 -0600

    Styled intro and most of details page

--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -1,60 +1,141 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {useParams} from 'react-router-dom';
-import styled from 'styled-components';
-import SocialCard from '../DetailsPage/SocialCard';
-import SelectProgramCard from '../DetailsPage/SelProgCard';
-import ProblemCard from '../DetailsPage/ProblemCard';
-import DiscoverCard from '../DetailsPage/DiscoverCard';
-import ContributeCard from '../DetailsPage/ContributeCard';
+import styled, {keyframes} from 'styled-components';
+
+import Background from '../../images/bkg-forest-top.jpg';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBullhorn, faScroll, faSeedling, faExclamation, faHandsHelping } from '@fortawesome/free-solid-svg-icons';
+
+const ActionData = [
+  {text: `Share with Friends`, icon: faBullhorn},
+  {text: `View other energy programs near you`, icon: faScroll},
+  {text: `Discover Green Energy's Impact`, icon: faSeedling},
+  {text: `Report a problem with this energy program`, icon: faExclamation},
+  {text: `Discover how you can contribute`, icon: faHandsHelping}
+];
 
 const Container = styled.div`
-width: auto;
-height: auto;
-margin: 0 auto;
-display: flex;
-flex-direction: column;
-justify-content: center;
+  width: auto;
+  height: 100vh;
+  margin: 0 auto;
+  display: grid;
+  grid-template-areas: "main" "action";
+  grid-template-rows: 1fr auto;
+  background-image: url(${Background});
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center;
+  overflow: hidden;
+`;
+
+const appear_step1 = keyframes`
+  0% {opacity: 0;}
+  100% {opacity: 1;}
+`;
+const appear_step2 = keyframes`
+  0% {opacity: 0;}
+  50% {opacity: 0;}
+  100% {opacity: 1;}
+`;
+const appear_step3 = keyframes`
+  0% {bottom: -100%;}
+  75% {bottom: -100%;}
+  100% {bottom: 0;}
 `;
 
 const TitleDiv = styled.div`
-width: 100%;
-height: 250px;
-margin: 0 auto;
-display: block;
-text-align: center;
-justify-content: center;
+  grid-area: main;
+  text-align: center;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  text-shadow: 0 0 4px black;
+  h1 {
+    font-family: var(--font-header);
+    font-size: 96px;
+    margin: 0;
+    margin-bottom: 32px;
+  }
+  h2 {
+    font-family: var(--font-main);
+    margin: 8px;
+    font-size: 16px;
+  }
+  h3 {
+    font-family: var(--font-main);
+    margin: 4px;
+    font-size: 24px;
+  }
+  p {
+    margin: 0;
+    font-family: var(--font-main);
+  }
+  
+  
 `;
+
+
 
 const BottomDisplay = styled.div`
-width: 300x;
-height: 200px;
-display: flex;
-margin: px auto;
-padding-right: px;
-justify-content: center;
-align-content: center;
+  grid-area: action;
+  display: flex;
+  margin: px auto;
+  padding-right: px;
+  justify-content: center;
+  align-content: center;
+  backdrop-filter: blur(4px);
+  border-top: 4px dashed white;
+  position: relative;
+  animation: 3s ${appear_step3} ease-in;
 `;
 
 
 
-// BEGIN bottom section component //
-function BottomDash (details) {
-  
-  
 
-  return(
-  <BottomDisplay>
-      <SocialCard />
-      <SelectProgramCard />
-      <ProblemCard />
-      <DiscoverCard />
-      <ContributeCard />
-  </BottomDisplay> 
-  )
-};
-//END bottom component // 
+const ActionCard = styled.button`
+  
+  text-align: center;
+  width: 20%;
+  padding: 16px;
+  background-color: rgba(255,255,255,.1);
+  color: rgba(255,255,255,.8);
+  transition: all .3s;
+  border: none;
+  font-size:  1em;
+  outline: none;
+  
+  p {
+    font-family: var(--font-main);
+    text-shadow: 0 0 8px black;
+  }
+  .icon {
+    display: block;
+    margin: 32px auto;
+    font-size: 48px;
+    transition: transform 1s;
+  }
+  &:hover, &:focus {
+    cursor: pointer;
+    background-color: rgba(255,255,255,.2);
+    color: rgba(255,255,255,1);
+    .icon {
+      transform: scale(1.4);
+    }
+  }
 
+`;
+
+const TitleMain = styled.div`
+  animation: 1s ${appear_step1} ease-in;
+`;
+const TitleAction = styled.div`
+  animation: 2s ${appear_step2} ease-in;
+`;
 
 
 export default function DetailsPage() {
@@ -68,22 +149,39 @@ export default function DetailsPage() {
   useEffect(()=>{
     dispatch({type: 'GET_PROGRAM_DETAILS', payload: id});
   }, [id]);
+
+  function renderActions() {
+    return ActionData.map( (item,i)=>{
+      return (
+        <ActionCard >
+          <FontAwesomeIcon className="icon" icon={item.icon}/>
+          <p>{item.text}</p>
+        </ActionCard>
+      )
+    });
+  }
   
 
   return(
     <Container>
-      {JSON.stringify(details)}
       <TitleDiv>
-        <h3>{details.utility_name} - {details.eiaid}</h3>
-        <h1>{details.program_name}</h1>
-        <p>You're one step closer to green energy.</p>
-        <p>Continue to the program website to sign up!</p>
-        <a href = {details.sign_up_url}>
-          <button class= "button-primary" > Go!</button>
-        </a>
+        <TitleMain>
+          <h2>{details.utility_name} - {details.eiaid}</h2>
+          <h1>{details.program_name}</h1>
+          <h3>You're one step closer to green energy.</h3>
+        </TitleMain>
+        <TitleAction>
+          <p>Continue to the program website to sign up!</p>
+          <a href = {details.sign_up_url}>
+            <button class= "button-primary" > Go!</button>
+          </a>
+        </TitleAction>
       </TitleDiv>
-      <BottomDash>
-      </BottomDash>
+
+      <BottomDisplay>
+        {renderActions()}
+      </BottomDisplay> 
+
     </Container>
   )
 }
