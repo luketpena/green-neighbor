caaef14ce2a42e8a2f3cc887df7a06b0c6211b96
Luke Peña <luketpena@gmail.com>
2020-02-06 13:35:36 -0600

    Created home button

---
+++ src/components/HomeButton/HomeButton.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import { FontAwesomeIcon}  from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
+const Container = styled.div`
+  font-size: 4em;
+  color: white;
+  display: flex;
+  align-items: flex-end;
+  text-shadow: 0 0 8px black;
+  padding: 16px;
+
+  .icon {
+
+    transition: transform .2s;
+    transition-timing-function: cubic-bezier(.17,.67,.67,2);
+    &:hover {
+      transform: scale(1.2);
+      cursor: pointer;
+    }
+  }
+  h1 {
+    display: inline;
+    font-size: .5em;
+    margin: 0;
+    margin-left: 16px;
+    font-family: var(--font-header);
+  }
+`;
+
+export default function HomeButton() {
+
+  const history = useHistory();
+
+  return (
+    <Container>
+      <FontAwesomeIcon className="icon" icon={faHome} onClick={()=>history.push('/intro')}/>
+      <h1>The Green Neighbor Challenge</h1>
+    </Container>
+  )
+}
