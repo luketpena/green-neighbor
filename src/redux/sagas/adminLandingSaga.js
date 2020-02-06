ebdeec24161f605503483816e2cac214c36f831f
dmercertaylor <dmercertaylor01@gmail.com>
2020-02-06 11:17:29 -0600

    tried creating createSaga

--- src/redux/sagas/adminLandingSaga.js
+++ src/redux/sagas/adminLandingSaga.js
@@ -4,9 +4,7 @@ import axios from 'axios';
 // type: GET_PROGRAMS, payload: string of the zip code you want to get programs for
 function* getAdminLandingData(action) {
   try{
-    const name = yield axios.get(
-        `/api/utilities/api/admin/overview`
-    );
+    const name = yield axios.get(`/api/utilities/api/admin/overview`);
     yield put({type: 'SET_ADMIN_LANDING_DATA', payload: name.data});
   } catch (error) {
       console.log(error);
