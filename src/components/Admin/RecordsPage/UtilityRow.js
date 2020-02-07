0d8f1b7cac582b403a105511ee2ed23dffd51d92
Luke Peña <luketpena@gmail.com>
2020-02-07 10:32:00 -0600

    Merged to master

--- src/components/Admin/RecordsPage/UtilityRow.js
+++ src/components/Admin/RecordsPage/UtilityRow.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useDispatch} from 'react-redux';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -10,11 +11,21 @@ const Container = styled.div`
 const InfoBox = styled.div`
   display: grid;
   grid-template-columns: 4fr 1fr 1fr 1fr 1fr;
+  text-align: center;
   p {
     margin: 0 8px;
   }
+  p:first-child {
+    text-align: left;
+  }
   .production {
-    color: ${props=>(props.active? 'var(--color-primary-light)' : '#AAA')}
+    color: ${props=>(props.active? 'var(--color-primary)' : '#AAA')};
+    transition: all .2s;
+    &:hover {
+      color: ${props=>(props.active? 'var(--color-primary-bright)' : '#666')};
+      transform: scale(1.2);
+      cursor: pointer;
+    }
   }
 `;
 
@@ -24,7 +35,17 @@ const ButtonBox = styled.div`
 
 export default function UtilityRow(props) {
 
+  const dispatch = useDispatch();
+
   const {id, utility_name, zip, state, program_count, production} = props.utility;
+
+  function toggleProduction() {
+    const myPayload = {
+      id,
+      production: !production
+    }
+    dispatch({type: 'SET_UTILITY_PRODUCTION', payload: myPayload})
+  }
 
   return (
     <Container className="utility-row">
@@ -33,7 +54,13 @@ export default function UtilityRow(props) {
         <p>{state}</p>
         <p>{zip}</p>
         <p>{program_count} {(program_count==1? 'program' : 'programs')}</p>
-        <p className="production" active={production}>{(production? 'Live' : 'Inactive')}</p>
+        <p 
+          className="production" 
+          active={production}
+          onClick={toggleProduction}
+        >
+          {(production? 'Live' : 'Inactive')}
+        </p>
       </InfoBox>
       <ButtonBox>
         <button className="button-default">Details</button>
