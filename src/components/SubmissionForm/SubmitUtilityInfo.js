c8bd3dc6e94c287efc069bc64b656b41e6045f48
Luke Peña <luketpena@gmail.com>
2020-02-12 13:42:26 -0600

    Zips delete popup added with MUI dialog

--- src/components/SubmissionForm/SubmitUtilityInfo.js
+++ src/components/SubmissionForm/SubmitUtilityInfo.js
@@ -3,6 +3,12 @@ import styled from 'styled-components';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faTimes} from '@fortawesome/free-solid-svg-icons';
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -39,11 +45,22 @@ const ZipBox = styled.div`
   border-left: 2px dashed #DDD;
   box-sizing: border-box;
   padding: 16px;
+
+
   ul {
     list-style: none;
     text-align: left;
     display: inline-block;
     padding: 0;
+    height: 350px;
+
+    background-color: #DDD;
+    border-radius: 8px;
+    box-shadow: inset 0 1px 0px 1px rgba(0,0,0,.1);
+    padding: 8px 16px;
+    width: 200px;
+
+    
 
     button {
       border: none;
@@ -97,6 +114,11 @@ const EiaBox = styled.div`
   display: inline-block;
 `;
 
+const PopUpContent = styled.div`
+  max-width: 500px;
+  width: 100%;
+`;
+
 const states = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
 
 export default function SubmitUtilityInfo() {
@@ -106,6 +128,8 @@ export default function SubmitUtilityInfo() {
   const [state,setState] = useState('State');
   const [utility_name, setUtility_name] = useState('');
   const [eiaid,setEiaid] = useState('');
+
+  const [deleteZip, setDeleteZip] = useState(-1);
 
   const [comm_bundled, setComm_bundled] = useState('');
   const [comm_delivery, setComm_delivery] = useState('');
@@ -130,7 +154,7 @@ export default function SubmitUtilityInfo() {
     return zips.map( (item,i)=>{
       return (
       <li key={i}>
-        <button onClick={()=>removeZip(i)}><FontAwesomeIcon icon={faTimes}/></button>
+        <button onClick={()=>setDeleteZip(i)}><FontAwesomeIcon icon={faTimes}/></button>
         {item} 
         </li>
       );
@@ -143,9 +167,10 @@ export default function SubmitUtilityInfo() {
     setZipInput('');
   }
 
-  function removeZip(index) {
+  function removeZip() {
     let copy = [...zips];
-    copy.splice(index,1);
+    copy.splice(deleteZip,1);
+    setDeleteZip(-1);
     setZips(copy);
   }
 
@@ -216,6 +241,15 @@ export default function SubmitUtilityInfo() {
           {renderZips()}
         </ul>
       </ZipBox>
+
+      <Dialog open={(deleteZip!==-1)}>
+        <PopUpContent>
+          <DialogTitle>{(deleteZip!==-1? <span>Delete ZIP {zips[deleteZip]}?</span> : <span>Delete ZIP</span>)}</DialogTitle>
+        
+          <button className="button-negative" onClick={removeZip}>Delete</button>
+          <button className="button-default" onClick={()=>setDeleteZip(-1)}>Cancel</button>
+        </PopUpContent>
+      </Dialog>
 
     </Container>
   )
