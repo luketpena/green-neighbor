704bd87c7d73c85fc62a77cf00768b8b105b46c2
dmercertaylor <dmercertaylor01@gmail.com>
2020-02-12 15:27:20 -0600

    blocks now display

--- src/components/SubmissionForm/PricingForm.js
+++ src/components/SubmissionForm/PricingForm.js
@@ -13,9 +13,13 @@ export default function PricingForm(props){
     const [costIsRange, setCostIsRange] = useState(form.cost_range ? 1 : 0);
     const [creditYesNo, setCreditYesNo] = useState(form.credit_yn || 'No');
     const [creditKwh, setCreditKwh] = useState(form.credit_kwh || '');
+    const [blocksAvailable, setBlocksAvailable] = useState(form.blocks_available || 'No');
+    const [blocksAvailableMinMaxValue, setBlocksAvailableMinMaxValue] = useState(2);
+    const [blocks, setBlocks] = useState(form.block_size_kwh && form.block_size_kwh.split(';') || []);
+    const [newBlockSizeKwh, setNewBlockSizeKwh] = useState('');
 
     const updateSubmissionForm = obj => {
-        dispatch({type: 'UDATE_SUBMISSION_FORM', payload: obj});
+        dispatch({type: 'UPDATE_SUBMISSION_FORM', payload: obj});
     }
 
     const costModelToggle = e => {
@@ -26,6 +30,29 @@ export default function PricingForm(props){
         } else {
             updateSubmissionForm({cost_kwh: undefined});
         }
+    }
+
+    const updateBlocksAvailable = e => {
+        let valueToSend = blocksAvailable;
+        if(blocksAvailable === 'Min' || blocksAvailable === 'Max'){
+            valueToSend += ` ${blocksAvailableMinMaxValue}`;
+        }
+        updateSubmissionForm({blocks_available: valueToSend});
+    }
+
+    const addBlock = e => {
+        e.preventDefault();
+        if(newBlockSizeKwh === '') return;
+        blocks.push(newBlockSizeKwh);
+        updateSubmissionForm({block_size_kwh: blocks.join(';')});
+        setBlocks(blocks);
+        setNewBlockSizeKwh('');
+    }
+
+    const removeBlock = (index) => {
+        blocks.splice(index, 1);
+        updateSubmissionForm({block_size_kwh: blocks.join(';')})
+        setBlocks(blocks);
     }
 
     return (
@@ -63,8 +90,7 @@ export default function PricingForm(props){
                             cost_range:`${costRangeMin}-${costRangeMax}`
                         })}
                     />
-                </>:
-                <>
+                </>:<> {/* END costIsRange === true, BEGIN costIsRange===false */}
                     <label htmlFor='submission-cost-kwh'>Cost kWh</label>
                     <input
                         id='submission-cost-kwh'
@@ -93,12 +119,60 @@ export default function PricingForm(props){
                     <label htmlFor='submission-credit-kwh'>Credit / kWh: </label>
                     <input
                         id='submission-credit-kwh'
-                        value='creditKwh'
+                        value={creditKwh}
                         type="number"
                         onChange={e => setCreditKwh(e.target.value)}
                         onBlur={e=>updateSubmissionForm({credit_kwh: creditKwh})}
                         placeholder='2.99'
                     />
+                </>
+            }
+            <label htmlFor='submission-blocks-available'>Blocks Available: </label>
+            <select
+                id='submission-blocks-available'
+                value={blocksAvailable}
+                onChange={e=>setBlocksAvailable(e.target.value)}
+                onBlur={updateBlocksAvailable}
+            >
+                <option>No</option>
+                <option>Fixed</option>
+                <option>Any</option>
+                <option>Min</option>
+                <option>Max</option>
+            </select>
+            {(blocksAvailable === 'Min' || blocksAvailable === 'Max') && 
+                <>
+                    <label htmlFor='submission-blocks-available-min-max'>
+                        {blocksAvailable} Blocks:&nbsp;
+                    </label>
+                    <input
+                        id='submission-blocks-available-min-max'
+                        value={blocksAvailableMinMaxValue}
+                        onChange={e=>setBlocksAvailableMinMaxValue(e.target.value)}
+                        onBlur={updateBlocksAvailable}
+                    />
+                </>
+            }
+            {blocksAvailable!=='No' &&
+                <>
+                    <p>Blocks (kWh):</p>
+                    {blocks.map((block, i) => 
+                        <button key={i} onClick={()=>removeBlock(i)} >
+                            {block}
+                        </button>
+                    )}
+                    <form onSubmit={addBlock}>
+                        <label htmlFor='submission-add-block'>
+                            Block Size (kWh):
+                        </label>
+                        <input
+                            id='submission-add-block'
+                            type='number'
+                            value={newBlockSizeKwh}
+                            onChange={e=>setNewBlockSizeKwh(e.target.value)}
+                        />
+                        <button type='submit'>Add Block</button>
+                    </form>
                 </>
             }
         </div>
