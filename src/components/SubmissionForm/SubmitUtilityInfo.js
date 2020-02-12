74c3e7ee1ff3031af0a483888197f8cd9e64d075
Luke Peña <luketpena@gmail.com>
2020-02-12 13:14:48 -0600

    All inputs in place and set to controlled

--- src/components/SubmissionForm/SubmitUtilityInfo.js
+++ src/components/SubmissionForm/SubmitUtilityInfo.js
@@ -1,21 +1,100 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
 
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   width: 100%;
   height: 100%;
+  input {
+    text-align: center;
+  }
+
+  label {
+    text-align: center;
+  }
+
+  select {
+    max-width: 100px;
+  }
 `;
 
 const BasicBox = styled.div`
-  background-color: red;
   min-width: 50%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  padding: 16px;
+  box-sizing: border-box;
+  .rate-title {
+    text-align: center;
+  }
 `;
 
 const ZipBox = styled.div`
-  background-color: blue;
   min-width: 50%;
+  text-align: center;
+  border-left: 2px dashed #DDD;
+  box-sizing: border-box;
+  padding: 16px;
+  ul {
+    list-style: none;
+    text-align: left;
+    display: inline-block;
+    padding: 0;
+
+    button {
+      border: none;
+      outline: none;
+      cursor: pointer;
+      background: none;
+      color: gray;
+      &:hover {
+        color: red;
+      }
+    }
+  }
+  
+`;
+
+const RateBox = styled.div`
+  display: grid;
+  grid-template-areas: "t1 t2" "f1 f2";
+  text-align: center;
+  margin-bottom: 16px;
+  background-color: #DDD;
+  border-radius: 8px;
+  box-shadow: inset 0 1px 0px 1px rgba(0,0,0,.1);
+  padding: 8px 0;
+
+  input {
+    margin: 0 8px;
+    
+  }
+  
+
+  label:first-child {
+    grid-area: t1;
+  }
+  label:nth-child(2) {
+    grid-area: t2;
+  }
+  input:first-child {
+    grid-area: f1;
+  }
+  input:nth-child(2) {
+    grid-area: f2;
+  }
+`;
+
+const LocationBox = styled.div`
+  margin: 16px 0;
+`;
+
+const EiaBox = styled.div`
+  display: inline-block;
 `;
 
 const states = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
@@ -23,7 +102,19 @@ const states = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM
 export default function SubmitUtilityInfo() {
 
   const [zipInput, setZipInput] = useState('');
-  const [zips, setZips] = useState([]);
+  const [zips, setZips] = useState([555,555,555]);
+  const [state,setState] = useState('State');
+  const [utility_name, setUtility_name] = useState('');
+  const [eiaid,setEiaid] = useState('');
+
+  const [comm_bundled, setComm_bundled] = useState('');
+  const [comm_delivery, setComm_delivery] = useState('');
+
+  const [ind_bundled, setInd_bundled] = useState('');
+  const [ind_delivery, setInd_delivery] = useState('');
+
+  const [res_bundled, setRes_bundled] = useState('');
+  const [res_delivery, setRes_delivery] = useState('');
 
   function renderStates() {
     return states.map( (item,i)=>{
@@ -37,7 +128,12 @@ export default function SubmitUtilityInfo() {
 
   function renderZips() {
     return zips.map( (item,i)=>{
-      return <li key={i}>{item} <button onClick={()=>removeZip(i)}>x</button></li>
+      return (
+      <li key={i}>
+        <button onClick={()=>removeZip(i)}><FontAwesomeIcon icon={faTimes}/></button>
+        {item} 
+        </li>
+      );
     })
   }
 
@@ -57,16 +153,56 @@ export default function SubmitUtilityInfo() {
     <Container>
 
       <BasicBox>
-        <label>Utility Name</label>
-        <input type="text" />
+        <h3>Basic Info</h3>
+        <label>Utility Name: </label>
+        <input 
+          type="text" 
+          placeholder="Enter the name of the utility company"
+          value={utility_name}
+          onChange={event=>setUtility_name(event.target.value)}
+          />
+        
+        <LocationBox>
+          <EiaBox>
+            <label>EIA ID: </label>
+            <input 
+              type="text" 
+              placeholder="Enter the EIA ID"
+              value={eiaid}
+              onChange={event=>setEiaid(event.target.value)}
+              />
+          </EiaBox>
 
-        <select>
-          <option disabled>State</option>
-          {renderStates()}
-        </select>
+          <select value={state} onChange={event=>setState(event.target.value)}>
+            <option disabled>State</option>
+            {renderStates()}
+          </select>
+        </LocationBox>
+        
 
-        <label>EIAID</label>
-        <input type="text" />
+        <label className="rate-title">Commercial Rates</label>
+        <RateBox>
+          <label>Bundled</label>
+          <input type="number" value={comm_bundled} onChange={event=>setComm_bundled(event.target.value)}/>
+          <label>Delivery</label>
+          <input type="number" value={comm_delivery} onChange={event=>setComm_delivery(event.target.value)}/>
+        </RateBox>
+
+        <label className="rate-title">Individual Rates</label>
+        <RateBox>
+          <label>Bundled</label>
+          <input type="number" value={ind_bundled} onChange={event=>setInd_bundled(event.target.value)}/>
+          <label>Delivery</label>
+          <input type="number" value={ind_delivery} onChange={event=>setInd_delivery(event.target.value)}/>
+        </RateBox>
+
+        <label className="rate-title">Residential Rates</label>
+        <RateBox>
+          <label>Bundled</label>
+          <input type="number" value={res_bundled} onChange={event=>setRes_bundled(event.target.value)}/>
+          <label>Delivery</label>
+          <input type="number" value={res_delivery} onChange={event=>setRes_delivery(event.target.value)}/>
+        </RateBox>
       </BasicBox>
 
       <ZipBox>
