8997fe8e1dbaebb4282c3cf676e0d8ae4ec58edc
Ben Stretar <ben.stretar@gmail.com>
2020-02-13 11:57:20 -0600

    monthly min YesNo selector

--- src/components/SubmissionForm/ContractForm.js
+++ src/components/SubmissionForm/ContractForm.js
@@ -14,6 +14,7 @@ export default function PricingForm(props){
     const dispatch = useDispatch();
 
     const [length, setLength] = useState('');
+    const [minYesNo, setminYesNo] = useState('');
     const [minimum, setMinimum] = useState('');
     const [termination, setTermination] = useState('');
     const [termination_cost, setTermination_cost] = useState('');
@@ -26,40 +27,64 @@ export default function PricingForm(props){
     return (
         <Container>
             <h2>Contract</h2>
+            <div>
                 <label>Length: </label>
-                <input
-                    type="text"
-                    value={length}
-                    onChange={e=>setLength(e.target.value)}
-                    onBlur={e=>updateSubmissionForm({contract_length: length})}
-                />
-                <label>Minimum: </label>
+                    <input
+                        type="text"
+                        value={length}
+                        placeholder="12-month"
+                        onChange={e=>setLength(e.target.value)}
+                        onBlur={e=>updateSubmissionForm({contract_length: length})}
+                    />
+                </div>
+                <div>
+                <label>Is there a Monthly Minimum? </label>
+                <select 
+                value={minYesNo}
+                onChange={e=>setminYesNo(e.target.value)}
+                onBlur={e=>updateSubmissionForm({monthly_min: minYesNo})}
+                >
+                    <option>Pick One</option>
+                    <option value={'No'}>No</option>
+                    <option value={'Yes'}>Yes</option>
+                </select>
+                {minYesNo === 'Yes' &&
+                <>
+                <label> Monthly Minimum: </label>
                 <input
                     type="text"
                     value={minimum}
+                    placeholder="12-month"
                     onChange={e=>setMinimum(e.target.value)}
                     onBlur={e=>updateSubmissionForm({monthly_min: minimum})}
                 />
-                <label>Termination Fee: </label>
+                </>
+                }
+                </div>
+                <div>
+                <label>Is there a Termination Fee? </label>
                 <select
                     value={termination}
                     onChange={e=>setTermination(e.target.value)}
                     onBlur={e=>updateSubmissionForm({termination_fee: termination})}
                 >
+                    <option>Pick One</option>
                     <option value={'No'}>No</option>
                     <option value={'Yes'}>Yes</option>
                 </select>
                 {termination === 'Yes' &&
                 <>
-                    <label>Termination Cost: </label>
+                    <label> Termination Cost: </label>
                     <input
                         type="text"
                         value={termination_cost}
+                        placeholder="150"
                         onChange={e=>setTermination_cost(e.target.value)}
                         onBlur={e=>updateSubmissionForm({termination_cost: termination_cost})}
                     />
                 </>
                 }
+                </div>
         </Container>
     )
 }
