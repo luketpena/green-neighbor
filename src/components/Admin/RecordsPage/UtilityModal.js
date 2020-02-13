961713cec7bf6aab05782617e56d47a2978afc27
dmercertaylor <dmercertaylor01@gmail.com>
2020-02-13 14:06:44 -0600

    added add program button that pushes to create page

--- src/components/Admin/RecordsPage/UtilityModal.js
+++ src/components/Admin/RecordsPage/UtilityModal.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
+import {useHistory} from 'react-router-dom';
 import styled from 'styled-components';
 
 import Dialog from '@material-ui/core/Dialog';
@@ -61,84 +62,102 @@ function Program({program}){
 }
 
 export default function UtilityModal(props){
-    const dispatch = useDispatch();
-    const utility = useSelector(state => state.adminRecordsModalUtility);
-    const {
-      eia_state, utility_name,
-      zips, 
-      programs, utility_id
-    } = utility;
-    const [production, setProduction] = useState(utility.production);
-    useEffect(()=>{
-      setProduction(utility.production);
-    }, [utility.production]);
 
-    const open = useSelector(state => state.adminRecordsModalOpen);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const utility = useSelector(state => state.adminRecordsModalUtility);
+  const {
+    eia_state, utility_name,
+    zips, 
+    programs, utility_id
+  } = utility;
+  const [production, setProduction] = useState(utility.production);
+  useEffect(()=>{
+    setProduction(utility.production);
+  }, [utility.production]);
 
-    const close = () => {
-        dispatch({type: 'SET_ADMIN_RECORDS_MODAL_OPEN', payload: false});
-    }
+  const open = useSelector(state => state.adminRecordsModalOpen);
 
-    const toggleProduction = () => {
-      dispatch({
-        type: 'SET_UTILITY_PRODUCTION',
-        payload: {
-          id: utility_id,
-          production: !production,
-          page: props.page,
-          search: props.search
-        }
-      });
-      setProduction(!production);
-    } 
+  const close = () => {
+      dispatch({type: 'SET_ADMIN_RECORDS_MODAL_OPEN', payload: false});
+  }
 
-    const openEdit = () => {
-      dispatch({type: 'GET_EDIT_INFO_UTILITY', payload: utility_id});
-    }
- 
-    return (
-        <Dialog
-            open={open}
-            onClose={close}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-            className="dialog"
-        >
-        <DialogTitle id="alert-dialog-title">
-          <ProgramTitle>
-            Programs for {utility_name} -
-            <ProductionButton
-              live={production}
-              onClick={toggleProduction}
-            >
-              {production ? 'Active' : 'Inactive'}
-            </ProductionButton>
-            -
-            <button className='button-default' onClick={openEdit}>
-              Edit
-            </button>
-          </ProgramTitle>
-        </DialogTitle>
-        <DialogContent>
-          <p>EIA - State: {eia_state}</p>
-          <h3>Programs:</h3>
-          <List>
-            {programs && programs.map(program =>
-              <Program program={program} key={program.id} />
-            )}
-          </List>
-          <h3>Active Zips:</h3>
-          <List>
-            <p>
-              {zips && zips.map(a => a.zip).join(', ')}
-            </p>
-          </List>
-        </DialogContent>
-        <DialogActions>
-          <button className="button-default" onClick={close} >
-            Close
+  const toggleProduction = () => {
+    dispatch({
+      type: 'SET_UTILITY_PRODUCTION',
+      payload: {
+        id: utility_id,
+        production: !production,
+        page: props.page,
+        search: props.search
+      }
+    });
+    setProduction(!production);
+  } 
+
+  const openEdit = () => {
+    dispatch({type: 'GET_EDIT_INFO_UTILITY', payload: utility_id});
+  }
+
+  const addProgram = () => {
+    dispatch({
+      type: 'SET_SUBMISSION_FORM',
+      payload: {
+        eia_state,
+        utility_name,
+        eiaid: eia_state.match(/[0-9]*/)[0],
+        state: utility.state
+      }
+    });
+    history.push('/admin/submit/create/program');
+  }
+
+  return (
+    <Dialog
+      open={open}
+      onClose={close}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      className="dialog"
+    >
+      <DialogTitle id="alert-dialog-title">
+        <ProgramTitle>
+          Programs for {utility_name} -
+          <ProductionButton
+            live={production}
+            onClick={toggleProduction}
+          >
+            {production ? 'Active' : 'Inactive'}
+          </ProductionButton>
+          -
+          <button className='button-default' onClick={openEdit}>
+            Edit
           </button>
-        </DialogActions>
-        </Dialog>
-    )
+        </ProgramTitle>
+      </DialogTitle>
+      <DialogContent>
+        <p>EIA - State: {eia_state}</p>
+        <h3>Programs:</h3>
+        <List>
+          {programs && programs.map(program =>
+            <Program program={program} key={program.id} />
+          )}
+          <button className='button-default' onClick={addProgram}>
+            Add Program
+          </button>
+        </List>
+        <h3>Active Zips:</h3>
+        <List>
+          <p>
+            {zips && zips.map(a => a.zip).join(', ')}
+          </p>
+        </List>
+      </DialogContent>
+      <DialogActions>
+        <button className="button-default" onClick={close} >
+          Close
+        </button>
+      </DialogActions>
+    </Dialog>
+  )
 }
