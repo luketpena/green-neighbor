cc2eab78147fd509788e93a3a9208cd9d8105d09
Korey Peterson <korey.t.peterson@gmail.com>
2020-02-13 17:49:48 -0600

    cleaned up code spacing

--- src/components/Admin/ManageAdminsPage/NewAdmin.js
+++ src/components/Admin/ManageAdminsPage/NewAdmin.js
@@ -3,8 +3,6 @@ import {useDispatch, useSelector} from 'react-redux';
 import {useParams} from 'react-router-dom';
 import styled from 'styled-components';
 
-
-
 const Body = styled.form`
     h1 {
         font-family: var(--font-header);
@@ -31,49 +29,40 @@ const Body = styled.form`
 export default function NewAdmin() {
 
     const {usernames} = useParams();
-   
     const dispatch = useDispatch(); 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-   
-
-    
-
     const addNewAdmin = () => {
         dispatch({ type: 'POST_NEW_ADMIN', payload: {username, password} });
         dispatch({ type: 'GET_ADMIN_USERS', usernames});
-    }
-
+    };
 
     return(
       
-        
           <Body onSubmit={addNewAdmin}>
             <h1>Add New Admin</h1>
                 <form className="form">               
-                            <label htmlFor="name">New Name</label>
-                                <input 
-                                    type="text" 
-                                    placeholder="Username" 
-                                    id="name" 
-                                    value={username} 
-                                    onChange={e=>setUsername(e.target.value)} 
-                                    tabIndex="1">
-                                </input>                          
-                                <label htmlFor="password">Password</label>
-                                <input 
-                                    type="text" 
-                                    placeholder="Password" 
-                                    id="password" 
-                                    value={password} 
-                                    onChange={e=>setPassword(e.target.value)} 
-                                    tabIndex="2">   
-                                </input>                                  
-                            <button >Submit</button>                   
+                        <label htmlFor="name">New Name</label>
+                            <input 
+                                type="text" 
+                                placeholder="Username" 
+                                id="name" 
+                                value={username} 
+                                onChange={e=>setUsername(e.target.value)} 
+                                tabIndex="1">
+                            </input>                          
+                            <label htmlFor="password">Password</label>
+                            <input 
+                                type="text" 
+                                placeholder="Password" 
+                                id="password" 
+                                value={password} 
+                                onChange={e=>setPassword(e.target.value)} 
+                                tabIndex="2">   
+                            </input>                                  
+                        <button >Submit</button>                   
                 </form>
           </Body>
-          
-      
-    )
-}
+    );
+};
