18c5e11a8b30d06e5940f25047e75eca1d340420
dmercertaylor <dmercertaylor01@gmail.com>
2020-02-17 15:11:39 -0600

    updated router, DO NOT MERGE

---
+++ server/modules/utilitiesColumns.js
@@ -0,0 +1,13 @@
+module.exports = [
+    "id",
+    "eia_state",
+    "state",
+    "eiaid",
+    "utility_name",
+    "bundled_avg_comm_rate",
+    "bundled_avg_ind_rate",
+    "bundled_avg_res_rate",
+    "delivery_avg_comm_rate",
+    "delivery_avg_ind_rate",
+    "delivery_avg_res_rate",
+    "production"];
---
+++ server/modules/zipsColumns.js
@@ -0,0 +1,7 @@
+module.exports = [
+    "id",
+    "zip",
+    "eiaid",
+    "state",
+    "eia_state"
+];
