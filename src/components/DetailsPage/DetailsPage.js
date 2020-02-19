0744ac69c71faee8e0c1ced7d3f12cbf0ec5c00b
Luke Peña <luketpena@gmail.com>
2020-02-19 13:17:50 -0600

    Commented and cleaned up DetailsPage

--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -2,21 +2,15 @@ import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {useParams, useHistory} from 'react-router-dom';
 import styled, {keyframes} from 'styled-components';
-
-import Background from '../../images/bkg-forest-top.jpg';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faScroll, faSeedling, faExclamation, faHandsHelping, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 
+//-----< Resource Imports >-----\\
+import Background from '../../images/bkg-forest-top.jpg';
 import HomeButton from '../HomeButton/HomeButton';
 
-const ActionData = [
-  {action: 'utility', text: `View other energy programs near you`, short: 'Other Programs', icon: faScroll},
-  {action: 'about', text: `Discover Green Energy's Impact`, short: 'FAQ', icon: faSeedling},
-  {action: 'report', text: `Report a problem with this energy program`, short: 'Report Problem', icon: faExclamation},
-  {action: 'contribute', text: `Discover how you can contribute`, short: 'Contribute', icon: faHandsHelping}
-];
 
+//-----< Styling >-----\\
 const Container = styled.div`
   width: auto;
   height: 100vh;
@@ -31,12 +25,9 @@ const Container = styled.div`
   overflow: hidden;
 `;
 
-
-
 const TitleDiv = styled.div`
   grid-area: main;
   text-align: center;
-
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -63,11 +54,7 @@ const TitleDiv = styled.div`
     margin: 0;
     font-family: var(--font-main);
   }
-  
-  
 `;
-
-
 
 const BottomDisplay = styled.div`
   grid-area: action;
@@ -82,9 +69,7 @@ const BottomDisplay = styled.div`
   height: ${props=>(props.active? `200px` : `0px`)};
 `;
 
-
 const ActionCard = styled.button`
-  
   text-align: center;
   width: 25%;
   padding: 0 1%;
@@ -92,14 +77,11 @@ const ActionCard = styled.button`
   display: flex;
   flex-direction: column;
   align-items: center;
-
   transition: all .3s;
   border: none;
   outline: none;
-  font-size:  1em;
-  
+  font-size:  1em; 
   background: none;
-  
   p {
     font-family: var(--font-main);
     font-size: ${props=>(props.myWidth>500? '1em' : '.8em')};
@@ -172,7 +154,15 @@ const DiscoverBar = styled.div`
   }
 `;
 
+//>> This data populates the "discover more" bar, but the actual actions are handled on click
+const actionData = [
+  {action: 'utility', text: `View other energy programs near you`, short: 'Other Programs', icon: faScroll},
+  {action: 'about', text: `Discover Green Energy's Impact`, short: 'FAQ', icon: faSeedling},
+  {action: 'report', text: `Report a problem with this energy program`, short: 'Report Problem', icon: faExclamation},
+  {action: 'contribute', text: `Discover how you can contribute`, short: 'Contribute', icon: faHandsHelping}
+];
 
+//-----< Component Function >-----\\
 export default function DetailsPage() {
 
   const details = useSelector(state => state.programDetails)
@@ -182,18 +172,18 @@ export default function DetailsPage() {
   let [discoverActive, setDiscoverActive] = useState(false);  
   let [width, setWidth] = useState(window.innerWidth);
 
-
   useEffect(()=>{
     dispatch({type: 'GET_PROGRAM_DETAILS', payload: id});
     window.addEventListener('resize', ()=>setWidth(window.innerWidth));
   }, [id, dispatch]);
 
-
-
+  // This is used for blurring the 'discover more' buttons while tabbing through
   function blurActions(event) {
     event.target.blur();
   }
 
+  // This is what happens on clicking the 'discover more' buttons
+  // These cases are determined by the actionData array defined outside the component function
   function clickAction(action) {
     switch(action) {
       case 'utility': history.push(`/utility/${zip}`); break;
@@ -205,7 +195,7 @@ export default function DetailsPage() {
   }
 
   function renderActions() {
-    return ActionData.map( (item,i)=>{
+    return actionData.map( (item,i)=>{
       return (
         <ActionCard key={i} onMouseLeave={blurActions} onClick={()=>clickAction(item.action)} myWidth={width}>
           <FontAwesomeIcon className="icon" icon={item.icon}/>
@@ -215,10 +205,11 @@ export default function DetailsPage() {
     });
   }
   
-
   return(
     <Container>
+
       <HomeButton />
+      
       <TitleDiv>
         <TitleMain>
           <h2>{details.utility_name} - {details.eiaid}</h2>
